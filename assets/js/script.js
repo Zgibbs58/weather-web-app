b66bebcc6660539e644e2107f77b16b99cad05cd
Zgibbs58 <zacharywgibbs@gmail.com>
2023-05-20 00:17:46 -0500

    saved the city as the key in local storage and a value of the object with lat and lon.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,7 @@
 var lat;
 var lon;
-function getApi() {
-  // Insert the API url to get a list of your repos
-}
-
-getApi();
 var cityBtn = document.querySelector(".cityBtn");
+
 cityBtn.addEventListener("click", function getValue() {
   var city = document.querySelector(".input").value;
   var requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=8a0fe5b9b591db8dddd6997be68f3c34`;
@@ -17,7 +13,6 @@ cityBtn.addEventListener("click", function getValue() {
     .then(function (data) {
       lat = data[0].lat;
       lon = data[0].lon;
-      console.log(lat);
-      console.log(lon);
+      localStorage.setItem(city, JSON.stringify({ lat, lon }));
     });
 });
