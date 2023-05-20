f1a7353ee6dfdfa58ee25826e8ddac004043211c
Zgibbs58 <zacharywgibbs@gmail.com>
2023-05-20 00:51:11 -0500

    added logic to give a default city to application if no current city but if current city is not empty populat current city html with current city value.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,25 @@
-var lat;
-var lon;
+var lat = 36.1622767;
+var lon = -86.7742984;
+var city;
 var cityBtn = document.querySelector(".cityBtn");
+var currentCity = "";
+var defaultCity = "Nashville";
+console.log(currentCity);
+
+if (currentCity === "") {
+  document.querySelector(".cityName").textContent = defaultCity;
+} else {
+  var currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=8a0fe5b9b591db8dddd6997be68f3c34`;
+
+  fetch(currentWeatherUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      document.querySelector(".cityName").textContent = currentCity;
+    });
+}
 
 cityBtn.addEventListener("click", function getValue() {
   var city = document.querySelector(".input").value;
@@ -14,5 +33,7 @@ cityBtn.addEventListener("click", function getValue() {
       lat = data[0].lat;
       lon = data[0].lon;
       localStorage.setItem(city, JSON.stringify({ lat, lon }));
+      currentCity = city;
+      document.querySelector(".cityName").textContent = currentCity;
     });
 });
