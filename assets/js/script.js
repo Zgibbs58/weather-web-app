2f500a9f77977819574c8b28dd89da4805434257
Zgibbs58 <zacharywgibbs@gmail.com>
2023-05-20 00:06:20 -0500

    added click advent listener to grab the input value for city and run it through the geo api to get lat and lon for weather.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,13 @@ var lat;
 var lon;
 function getApi() {
   // Insert the API url to get a list of your repos
-  var requestUrl =
-    "http://api.openweathermap.org/geo/1.0/direct?q=nashville&limit=1&appid=8a0fe5b9b591db8dddd6997be68f3c34";
+}
+
+getApi();
+var cityBtn = document.querySelector(".cityBtn");
+cityBtn.addEventListener("click", function getValue() {
+  var city = document.querySelector(".input").value;
+  var requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=8a0fe5b9b591db8dddd6997be68f3c34`;
 
   fetch(requestUrl)
     .then(function (response) {
@@ -12,7 +17,7 @@ function getApi() {
     .then(function (data) {
       lat = data[0].lat;
       lon = data[0].lon;
+      console.log(lat);
+      console.log(lon);
     });
-}
-
-getApi();
+});
