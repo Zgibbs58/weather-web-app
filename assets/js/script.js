6ac3dc875a7d6c00c8a069bbab4e991f8b136f73
Zgibbs58 <zacharywgibbs@gmail.com>
2023-05-20 19:35:00 -0500

    changed currentCity variable to pull in last item added to local storage. If conditional checks for localStorage.length === 0 instead of null to fix bug. Moved currentWeather outside of click event and made it it's own function and added variables to target the html and update the temp element in html to show temp value.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,49 @@
-var defaultCity = "nashville";
-var lat = JSON.parse(localStorage.getItem(defaultCity));
-var lon = JSON.parse(localStorage.getItem(defaultCity));
+var defaultCity = "Nashville";
+var lat;
+var lon;
 var city;
 var cityBtn = document.querySelector(".cityBtn");
-var currentCity = localStorage.key([0]);
-console.log(currentCity);
+var currentCity = localStorage.key([localStorage.length - 1]);
+var currentTempEl = document.querySelector(".currentTemp");
+var currentWindEl = document.querySelector(".currentWind");
+var currentHumidEl = document.querySelector(".currentHumid");
 
-if (currentCity === null) {
+if (localStorage.length === 0) {
   document.querySelector(".cityName").textContent = defaultCity;
+  var lat = JSON.parse(localStorage.getItem(defaultCity));
+  var lon = JSON.parse(localStorage.getItem(defaultCity));
 } else {
+  currentCity = localStorage.key([localStorage.length - 1]);
   lat = JSON.parse(localStorage.getItem(currentCity)).lat;
   lon = JSON.parse(localStorage.getItem(currentCity)).lon;
   console.log(lat);
-  var currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=8a0fe5b9b591db8dddd6997be68f3c34`;
+  currentWeather();
+}
+
+cityBtn.addEventListener("click", function getValue(event) {
+  event.preventDefault();
+  var currentCity = document.querySelector(".input").value;
+  localStorage.removeItem(currentCity);
+
+  var requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${currentCity}&limit=1&appid=8a0fe5b9b591db8dddd6997be68f3c34`;
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      lat = data[0].lat;
+      lon = data[0].lon;
+      localStorage.setItem(currentCity, JSON.stringify({ lat, lon }));
+      document.querySelector(".cityName").textContent =
+        currentCity.toUpperCase();
+      currentWeather();
+    });
+});
+
+function currentWeather() {
+  var currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=8a0fe5b9b591db8dddd6997be68f3c34&units=imperial`;
 
   fetch(currentWeatherUrl)
     .then(function (response) {
@@ -20,23 +51,10 @@ if (currentCity === null) {
     })
     .then(function (data) {
       console.log(data);
-      document.querySelector(".cityName").textContent = currentCity;
+      document.querySelector(".cityName").textContent =
+        currentCity.toUpperCase();
+      currentTempEl.textContent = data.main.temp;
+      console.log(currentTempEl);
+      console.log(currentCity);
     });
 }
-
-cityBtn.addEventListener("click", function getValue() {
-  var city = document.querySelector(".input").value;
-  var requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=8a0fe5b9b591db8dddd6997be68f3c34`;
-
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      lat = data[0].lat;
-      lon = data[0].lon;
-      localStorage.setItem(city, JSON.stringify({ lat, lon }));
-      currentCity = city;
-      document.querySelector(".cityName").textContent = currentCity;
-    });
-});
