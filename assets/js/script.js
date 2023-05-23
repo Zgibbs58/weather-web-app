fecb71d7662424eca767217c0e79883149edebe4
Zgibbs58 <zacharywgibbs@gmail.com>
2023-05-23 13:09:36 -0500

    updated the js to set the data in local storage as an array of city objects and push the newData for new city to the end. Then all my lat, lon, and currentCity variables pull from the value of the last one entered.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,27 @@
 var apiKey = "8a0fe5b9b591db8dddd6997be68f3c34";
-var defaultCity = "NASHVILLE";
-localStorage.setItem(defaultCity, JSON.stringify({ lat: 36.1622767, lon: -86.7742984 }));
-var lat;
-var lon;
+var cityData = JSON.parse(localStorage.getItem("cityData")) || [];
+console.log(cityData);
+// localStorage.setItem(defaultCity, JSON.stringify({ lat: 36.1622767, lon: -86.7742984 }));
+var currentCity = cityData.length > 0 ? cityData[cityData.length - 1].city : null;
+var lat = cityData[cityData.length - 1].lat;
+var lon = cityData[cityData.length - 1].lon;
 var cityBtn = document.querySelector(".cityBtn");
-var currentCity = localStorage.key([localStorage.length - 1]);
 var currentTempEl = document.querySelector(".currentTemp");
 var currentWindEl = document.querySelector(".currentWind");
 var currentHumidEl = document.querySelector(".currentHumid");
 
-if (localStorage.length === 0) {
-  document.querySelector(".cityName").textContent = defaultCity;
-  lat = JSON.parse(localStorage.getItem(defaultCity)).lat;
-  lon = JSON.parse(localStorage.getItem(defaultCity)).lon;
+if (cityData.length === 0) {
+  document.querySelector(".cityName").textContent = "Search for a City to view your weather";
 } else {
-  lat = JSON.parse(localStorage.getItem(currentCity)).lat;
-  lon = JSON.parse(localStorage.getItem(currentCity)).lon;
-  console.log(currentCity);
+  //   lat = JSON.parse(localStorage.getItem(currentCity)).lat;
+  //   lon = JSON.parse(localStorage.getItem(currentCity)).lon;
+  //   console.log(currentCity);
   currentWeather();
 }
 
 cityBtn.addEventListener("click", function getValue(event) {
   event.preventDefault();
   var newCity = document.querySelector(".input").value.toUpperCase().trim();
-  localStorage.removeItem(newCity);
 
   var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + newCity + "&limit=1&appid=" + apiKey;
 
@@ -32,10 +30,17 @@ cityBtn.addEventListener("click", function getValue(event) {
       return response.json();
     })
     .then(function (data) {
-      localStorage.setItem(newCity.toUpperCase(), JSON.stringify({ lat, lon }));
-      console.log(data[0].state);
       lat = data[0].lat;
       lon = data[0].lon;
+      var newData = {
+        city: newCity,
+        lat: lat,
+        lon: lon,
+      };
+      cityData.push(newData);
+
+      localStorage.setItem("cityData", JSON.stringify(cityData));
+      console.log(data[0].state);
       document.querySelector(".cityName").textContent = newCity;
       currentCity = newCity;
       currentWeather();
@@ -43,6 +48,7 @@ cityBtn.addEventListener("click", function getValue(event) {
 });
 
 function currentWeather() {
+  console.log(cityData);
   var currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial";
 
   fetch(currentWeatherUrl)
